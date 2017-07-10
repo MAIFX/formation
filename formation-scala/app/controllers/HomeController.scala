9da647c0eb6826264e13be8f9a7b7c7ef3078117
Mathieu <mathieu.ancelin@gmail.com>
2017-07-10 14:04:51 +0200

    Add reactivemongo

--- formation-scala/app/controllers/HomeController.scala
+++ formation-scala/app/controllers/HomeController.scala
@@ -1,24 +1,128 @@
 package controllers
 
 import javax.inject._
-import play.api._
+
+import play.api.libs.json._
 import play.api.mvc._
+import play.modules.reactivemongo.ReactiveMongoApi
+import reactivemongo.api.Cursor
+import reactivemongo.api.indexes.{Index, IndexType}
+import reactivemongo.core.errors.DatabaseException
+import reactivemongo.play.json._
+import reactivemongo.play.json.collection._
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+import scala.concurrent.{ExecutionContext, Future}
+
+case class User(name: String, email: String)
+
+case class StoreError(msgs: List[String])
+
+object User {
+
+  val namespace = "mathieu.ancelin"
+
+  implicit val writes = Json.writes[User]
+  implicit val reads = Json.reads[User]
+
+  def users()(implicit mongo: ReactiveMongoApi, ec: ExecutionContext): Future[JSONCollection] =
+    mongo.database.map(_.collection[JSONCollection]("users-mathieu.ancelin"))
+
+  def addUser(user: User)(implicit mongoApi: ReactiveMongoApi, ec: ExecutionContext): Future[Either[StoreError, User]] = {
+    users().flatMap { coll =>
+      coll.insert[User](user).map {
+        case wr if wr.ok => Right(user)
+        case wr =>
+          val errors = wr.writeErrors.map(e => s"${e.code} => ${e.errmsg}").toList
+          Left(StoreError(errors))
+      } recover {
+        case d: DatabaseException => Left(StoreError(List(d.getMessage())))
+      }
+    }
+  }
+
+  def findAll()(implicit mongoApi: ReactiveMongoApi, ec: ExecutionContext): Future[Seq[User]] = {
+    users().flatMap { coll =>
+      coll.find(Json.obj())
+        .cursor[User]()
+        .collect[Seq](0, Cursor.FailOnError[Seq[User]]())
+    }
+  }
+
+  def findByEmail(email: String)(implicit mongoApi: ReactiveMongoApi, ec: ExecutionContext): Future[Option[User]] = {
+    users().flatMap { coll =>
+      coll
+        .find(Json.obj("email" -> email))
+        .one[User]
+    }
+  }
+
+}
+
 @Singleton
-class HomeController @Inject() extends Controller {
+class UserController @Inject()()(implicit mongo: ReactiveMongoApi, ec: ExecutionContext) extends Controller {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index = Action { implicit request =>
-    Ok(views.html.index())
+  import User._
+
+  mongo.database.map(_.collection[JSONCollection]("users-mathieu.ancelin")).flatMap { usrs =>
+    usrs.indexesManager.ensure(Index(Seq("email" -> IndexType.Ascending), unique = true))
+  }
+
+  def findByEmail(email: String) = Action.async { req =>
+    User.findByEmail(email)
+      .map { opt =>
+        opt.map(u => Ok(Json.toJson(u))).getOrElse(NotFound(Json.obj()))
+      }
+  }
+
+  def findAll() = Action.async { req =>
+    User.findAll().map { users =>
+      Ok(JsArray(users.map(u => Json.toJson(u))))
+    }
+  }
+
+  def createUser() = Action.async(parse.json) { req =>
+    req.body.validate[User] match {
+      case JsSuccess(user, _) =>
+        User.addUser(user).map {
+          case Right(usr) => Ok(Json.toJson(usr))
+          case Left(StoreError(errors)) => InternalServerError(Json.obj(
+            "errors" -> JsArray(errors.map(JsString.apply)))
+          )
+        }
+      case JsError(errors) => Future.successful(BadRequest(JsError.toJson(errors)))
+    }
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- formation-scala/build.sbt
+++ formation-scala/build.sbt
@@ -1,14 +1,15 @@
 name := """formation"""
 organization := "com.maifx"
-
 version := "1.0-SNAPSHOT"
 
-lazy val root = (project in file(".")).enablePlugins(PlayScala)
+lazy val root = (project in file("."))
+  .enablePlugins(PlayScala)
 
 scalaVersion := "2.11.11"
 
 libraryDependencies ++= Seq(
   ws,
+  "org.reactivemongo" %% "play2-reactivemongo" % "0.12.5-play25",
   "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.0" % Test
 )
 
