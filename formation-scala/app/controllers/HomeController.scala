aa29cd315c76e34525f5d34a12d0851f11cf05c5
Alexandre DELEGUE <alexandre.delegue@maif.fr>
2021-01-06 13:05:27 +0100

    Upgrade last version

--- formation-scala/app/controllers/HomeController.scala
+++ formation-scala/app/controllers/HomeController.scala
@@ -1,27 +1,35 @@
 package controllers
 
-import javax.inject._
+import akka.stream.Materializer
+import akka.stream.scaladsl.{Flow, Source}
+import akka.util.ByteString
+import controllers.User.users
 
+import javax.inject._
+import play.api.libs.json._
 import akka.{Done, NotUsed}
 import akka.stream._
 import akka.stream.scaladsl._
 import akka.util._
 import controllers.StatsActor.{StatsMessages, UserAdded, UserDeleted, UserRead}
 import play.api.Logger
-import play.api.libs.json._
 import play.api.libs.functional.syntax._
 import play.api.libs.streams.Accumulator
 import play.api.libs.ws.WSClient
 import play.api.mvc._
+import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
 import play.modules.reactivemongo.ReactiveMongoApi
 import reactivemongo.akkastream._
 import reactivemongo.api.Cursor
+import reactivemongo.api.bson.collection.BSONCollection
 import reactivemongo.api.indexes.{Index, IndexType}
 import reactivemongo.core.errors.DatabaseException
 import reactivemongo.play.json._
-import reactivemongo.play.json.collection._
+import reactivemongo.play.json.compat._
+import json2bson.{toDocumentReader, toDocumentWriter}
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Success
 
 object Implicits {
 
@@ -42,6 +50,8 @@ case class Geolocation(latitude: Double, longitude: Double)
 object Geolocation {
   implicit val geoFormat = Json.format[Geolocation]
 
+  val logger = Logger(Geolocation.getClass)
+
   def findGeoloc(ip: String)(implicit wSClient: WSClient, ec: ExecutionContext): Future[Option[Geolocation]] = {
     wSClient
       .url(s"https://freegeoip.net/json/${ip}")
@@ -51,7 +61,7 @@ object Geolocation {
           case 200 =>
             response.json.validate[Geolocation].fold(
               err => {
-                Logger.error(s"Oups : $err")
+                logger.error(s"Oups : $err")
                 None
               },
               success => Some(success)
@@ -62,7 +72,7 @@ object Geolocation {
       }
       .recover {
         case e =>
-          Logger.error(s"Oups : ", e)
+          logger.error(s"Oups : ", e)
           None
       }
   }
@@ -84,6 +94,7 @@ object User {
   implicit val writes = Json.writes[User]
   implicit val reads = Json.reads[User]
 
+
   val userToCSVByteString = Flow[User]
     .map(u => s"${u.email};${u.name}")
     .intersperse("user email;user name\n", "\n", "\n")
@@ -94,23 +105,23 @@ object User {
     materializer: Materializer,
     ec: ExecutionContext): Source[User, _] = {
 
-    Source.fromFuture(getUsersCollection()).flatMapConcat { coll =>
+    Source.future(users()).flatMapConcat { coll =>
       coll.find(Json.obj())
         .cursor[User]()
         .documentSource()
     }
   }
 
-  def getUsersCollection()(implicit mongo: ReactiveMongoApi, ec: ExecutionContext): Future[JSONCollection] =
-    mongo.database.map(_.collection[JSONCollection]("users-mathieu.ancelin"))
+  def users()(implicit mongo: ReactiveMongoApi, ec: ExecutionContext): Future[BSONCollection] =
+    mongo.database.map(_.collection[BSONCollection]("users-mathieu.ancelin"))
 
   def addUser(user: User)(implicit mongoApi: ReactiveMongoApi, ec: ExecutionContext): Future[Either[StoreError, User]] = {
-    getUsersCollection().flatMap { coll =>
-      coll.insert[User](user).map {
-        case wr if wr.ok => Right(user)
-        case wr =>
+    users().flatMap { coll =>
+      coll.insert.one(user).map {
+        case wr if wr.writeErrors.nonEmpty =>
           val errors = wr.writeErrors.map(e => s"${e.code} => ${e.errmsg}").toList
           Left(StoreError(errors))
+        case _ => Right(user)
       } recover {
         case d: DatabaseException => d.getMessage().toError[User]
       }
@@ -118,7 +129,7 @@ object User {
   }
 
   def findAll()(implicit mongoApi: ReactiveMongoApi, ec: ExecutionContext): Future[Seq[User]] = {
-    getUsersCollection().flatMap { coll =>
+    users().flatMap { coll =>
       coll.find(Json.obj())
         .cursor[User]()
         .collect[Seq](Int.MaxValue, Cursor.FailOnError[Seq[User]]())
@@ -126,11 +137,11 @@ object User {
   }
 
   def createStreamOld(stream: Source[User, _])(implicit mongoApi: ReactiveMongoApi, materializer: Materializer, ec: ExecutionContext): Source[JsObject, _] = {
-    Source.fromFuture(getUsersCollection()).flatMapConcat { coll =>
+    Source.future(users()).flatMapConcat { coll =>
       stream.mapAsyncUnordered(4) { user =>
-        coll.insert(user).map(wr =>
+        coll.insert.one(user).map(wr =>
           Json.obj(
-            "ok" -> wr.ok,
+            "ok" -> wr.writeErrors.isEmpty,
             "email" -> user.email
           )
         )
@@ -139,13 +150,13 @@ object User {
   }
 
   def createStream(stream: Source[User, _])(implicit mongoApi: ReactiveMongoApi, materializer: Materializer, ec: ExecutionContext): Source[JsObject, _] = {
-    Source.fromFuture(getUsersCollection()).flatMapConcat { coll =>
+    Source.future(users()).flatMapConcat { coll =>
       stream
         .mapAsync(20) { user =>
-          coll.insert[User](user)
+          coll.insert.one(user)
             .map(wr => Json.obj(
               "email" -> user.email,
-              "ok" -> wr.ok
+              "ok" -> wr.writeErrors.isEmpty
             ))
         }
     }
@@ -173,7 +184,7 @@ object User {
   }
 
   def findByEmail(email: String)(implicit mongoApi: ReactiveMongoApi, ec: ExecutionContext): Future[Option[User]] = {
-    getUsersCollection().flatMap { coll =>
+    users().flatMap { coll =>
       coll
         .find(Json.obj("email" -> email))
         .one[User]
@@ -184,11 +195,11 @@ object User {
     findByEmail(email).flatMap {
       case None => Future.successful(Left(StoreError(s"User not found for email $email")))
       case Some(user) => {
-        getUsersCollection().flatMap { coll =>
+        users().flatMap { coll =>
           coll
-            .update[JsObject, User](Json.obj("email" -> email), user)
+            .update.one(Json.obj("email" -> email), user)
             .map {
-              case wr if wr.ok => Right(user)
+              case wr if wr.writeErrors.isEmpty => Right(user)
               case wr =>
                 val errors = wr.writeErrors.map(e => s"${e.code} => ${e.errmsg}").toList
                 Left(StoreError(errors))
@@ -201,16 +212,19 @@ object User {
   }
 
   def deleteAll()(implicit mongoApi: ReactiveMongoApi, ec: ExecutionContext): Future[Either[StoreError, Done]] = {
-    getUsersCollection().flatMap { coll =>
-      coll.remove(Json.obj()).map {
-        case wr if wr.ok => Right(Done)
-        case wr =>
-          val errors = wr.writeErrors.map(e => s"${e.code} => ${e.errmsg}").toList
-          Left(StoreError(errors))
-      }
-      .recover {
-        case d: DatabaseException => Left(StoreError(d.getMessage()))
-      }
+    users().flatMap { coll =>
+      val builder = coll.delete(ordered = false)
+      builder.element(q = Json.obj(), limit = None, collation = None)
+        .flatMap( ops => builder.many(List(ops)) )
+        .map {
+          case wr if wr.writeErrors.isEmpty => Right(Done)
+          case wr =>
+            val errors = wr.writeErrors.map(e => s"${e.code} => ${e.errmsg}").toList
+            Left(StoreError(errors))
+        }
+        .recover {
+          case d: DatabaseException => Left(StoreError(d.getMessage()))
+        }
     }
   }
 
@@ -218,11 +232,11 @@ object User {
     findByEmail(email).flatMap {
       case None => Future.successful(Left(StoreError(s"User not found for email $email")))
       case Some(user) => {
-        getUsersCollection().flatMap { coll =>
+        users().flatMap { coll =>
           coll
-            .remove(Json.obj("email" -> email))
+            .delete.one(Json.obj("email" -> email))
             .map {
-              case wr if wr.ok => Right(user)
+              case wr if wr.writeErrors.isEmpty => Right(user)
               case wr =>
                 val errors = wr.writeErrors.map(e => s"${e.code} => ${e.errmsg}").toList
                 Left(StoreError(errors))
@@ -236,7 +250,10 @@ object User {
 }
 
 @Singleton
-class UserController @Inject()(statsService: StatsService)(implicit mongo: ReactiveMongoApi, wSClient: WSClient, materializer: Materializer, ec: ExecutionContext) extends Controller {
+class UserController @Inject()(statsService: StatsService,
+                               components: ControllerComponents,
+                               reactiveMongoApi: ReactiveMongoApi)
+                              (implicit ec: ExecutionContext, wSClient: WSClient, mat: Materializer)  extends AbstractController(components) {
 
   import User._
 
@@ -244,14 +261,17 @@ class UserController @Inject()(statsService: StatsService)(implicit mongo: React
     Accumulator.source[ByteString].map(Right.apply)
   }
 
-  mongo.database.map(_.collection[JSONCollection]("users-mathieu.ancelin")).flatMap { usrs =>
+  implicit val rmapi = reactiveMongoApi
+
+  reactiveMongoApi.database.map(_.collection[BSONCollection]("users-mathieu.ancelin")).flatMap { usrs =>
     usrs.indexesManager.ensure(Index(Seq("email" -> IndexType.Ascending), unique = true))
   }
 
   def findByEmail(email: String) = Action.async { req =>
     val findByEmail = User.findByEmail(email)
-    findByEmail.onSuccess {
-      case _ => statsService.sendEvent(UserRead)
+    findByEmail.onComplete {
+      case Success(_) => statsService.sendEvent(UserRead)
+      case _ =>
     }
     findByEmail
       .map { opt =>
--- formation-scala/build.sbt
+++ formation-scala/build.sbt
@@ -5,13 +5,14 @@ version := "1.0-SNAPSHOT"
 lazy val root = (project in file("."))
   .enablePlugins(PlayScala)
 
-scalaVersion := "2.11.11"
+scalaVersion := "2.13.4"
 
 libraryDependencies ++= Seq(
   ws,
-  "org.reactivemongo" %% "play2-reactivemongo" % "0.12.5-play25",
-  "org.reactivemongo" %% "reactivemongo-akkastream" % "0.12.5",
-  "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.0" % Test
+  guice,
+  "org.reactivemongo" %% "play2-reactivemongo" % "1.0.2-play28",
+  "org.reactivemongo" %% "reactivemongo-akkastream" % "1.0.2",
+  "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % Test
 )
 
 // Adds additional packages into Twirl
